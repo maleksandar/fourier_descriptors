d2263fad32d64ef089d67f4f8224d7f92f8bce25
aleksandar <alemilosav@gmail.com>
2018-09-23 22:16:17 +0200

    init

---
+++ main.py
@@ -0,0 +1,71 @@
+import numpy as np
+from numpy import fft
+from PIL import Image
+from matplotlib import pyplot as plt
+import math as math
+
+
+def menhetn_rastojanje(vec1, vec2):
+    return np.sum(np.abs(vec1 - vec2))
+
+def GFD(bw, m, n):
+    if len(bw.shape) > 2:
+        bw = bw.max(axis = 2) / 255
+    width = bw.shape[1]
+    # height = bw.shape[0]
+    N = width
+    maxR = math.sqrt((((N)//2)**2) + (((N)//2)**2))
+
+    x = np.linspace(-(N-1)//2, (N-1)//2, N )
+    y = x
+    X, Y = np.meshgrid(x, y)
+
+    radius = np.sqrt(np.power(X, 2) + np.power(Y, 2)) / maxR
+    print(radius)
+
+    theta = np.arctan2(Y, X)
+    theta[theta < 0] = theta[theta < 0] + 2+ np.pi
+    print(theta)
+
+    FR = np.zeros((m,n))
+    FI = np.zeros((m,n))
+    FD = np.zeros((m*n,1))
+
+    i = 0
+    for rad in range(m):
+        for ang in range(n):
+            # e^(i * theta) = cos(theta) + i * sin(theta)
+            tempR = bw * np.cos(2 * np.pi * rad * radius + ang * theta)
+            tempI = -1 * bw * np.sin(2 * np.pi * rad * radius + ang * theta)
+            FR[rad, ang] = np.sum(tempR)
+            FI[rad, ang] = np.sum(tempI)
+            
+            if rad == 0 and ang == 0:
+                FD[i] = math.sqrt((2* (FR[0,0] * FR[0,0]))) / (np.pi* maxR * maxR)
+            else:
+                FD[i] = math.sqrt((FR[rad, ang] * FR[rad, ang]) + (FI[rad, ang] * FI[rad, ang])) / (math.sqrt((2* (FR[0,0] * FR[0,0]))))
+            i = i + 1
+    return FD
+
+image_names = [
+    'facebook',
+    'instagram1',
+    'tennis1',
+    'twitter1'
+]
+
+images = [{ 'path':  'images/' + image + '.png', 'image': np.array(Image.open('./images/' + image + '.png')  ) } for image in image_names ]
+
+db = [ { 'path': image['path'], 'image': image['image'], 'descriptor': GFD(image['image'], 4, 9)} for image in images]
+
+
+def search(image_name):
+    stats = []
+    image = np.array(Image.open('images/'+image_name+'.png'))
+    gfd = GFD(image, 4, 9)
+    for db_image in db:
+        stats.append({'distance': menhetn_rastojanje(gfd, db_image['descriptor']), 'path': db_image['path'] })
+        stats.sort(key=distance)
+    return stats
+def distance(stat):
+    return stat['distance']
---
+++ web_server.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from main import search
+app = Flask(__name__, static_url_path='/static')
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/images/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('images', filename)
+
+@app.route('/search/<image_name>')
+def search_image(image_name):
+    stats = search(image_name)
+    return jsonify(stats[:4])
