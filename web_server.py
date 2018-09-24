ded99097cec3174f50804d68baea40bce66281c2
aleksandar <alemilosav@gmail.com>
2018-09-24 16:55:59 +0200

    changed url config

--- web_server.py
+++ web_server.py
@@ -22,4 +22,4 @@ def search_image(image_name):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(port=port)
